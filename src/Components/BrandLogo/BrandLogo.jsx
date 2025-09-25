fd38bb8a5a6e8e78c7b8f26e2b62c2a8a54544a2
sokakoyunlarifed-sudo <sokakoyunlarifed@gmail.com>
2025-09-25 21:56:20 +0300

    Sponsor Logolari

--- src/Components/BrandLogo/BrandLogo.jsx
+++ src/Components/BrandLogo/BrandLogo.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import { FaGamepad, FaRunning, FaWheelchair, FaTrophy, FaHandsHelping, FaHome } from "react-icons/fa";
+import React from "react";
+
+// 3 logoyu import et
+import AlfazLogo from "../assets/logos/Alfaz_Logo.png";
+import CankiriLogo from "../assets/logos/Cankiri_Logo.png";
+import PgLogo from "../assets/logos/Pg_Logo.png";
 
 const BrandLogo = () => {
   return (
-    <>
-    <div className='dark:bg-gray-900 dark:text-white'>
-    <div className='container py-12 '>
-        <h1 data-aos="fade-up" className='text-center'> Sokak oyunlarıyla <span className="text-primary">geleneklerimizi</span> yaşatıyoruz</h1>
-        <div data-aos="fade-up" data-aos-delay="300" className='flex flex-wrap animate-marquee items-center justify-evenly gap-3 py-6 md:px-100'>
-            <FaGamepad />
-    <FaRunning />
-    <FaWheelchair />
-    <FaTrophy />
-    <FaHandsHelping />
-    <FaHome />
+    <div className="dark:bg-gray-900 dark:text-white">
+      <div className="container py-12">
+        <h1 data-aos="fade-up" className="text-center">
+          Sokak oyunlarıyla{" "}
+          <span className="text-primary">geleneklerimizi</span> yaşatıyoruz
+        </h1>
+        <div
+          data-aos="fade-up"
+          data-aos-delay="300"
+          className="flex flex-wrap animate-marquee items-center justify-evenly gap-6 py-6 md:px-100"
+        >
+          <img
+            src={AlfazLogo}
+            alt="Alfaz Logo"
+            className="w-24 h-24 object-contain"
+          />
+          <img
+            src={CankiriLogo}
+            alt="Çankırı Logo"
+            className="w-24 h-24 object-contain"
+          />
+          <img
+            src={PgLogo}
+            alt="PG Logo"
+            className="w-24 h-24 object-contain"
+          />
         </div>
+      </div>
     </div>
-    </div>
-    </>
-  )
-}
+  );
+};
 
-export default BrandLogo
+export default BrandLogo;
